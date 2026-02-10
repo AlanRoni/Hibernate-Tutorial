079ff78b07e55fe32e5781aa0073c8ca605bf654
Alan <alanronimathew@gmail.com>
2026-02-10 23:45:22 +0400

    1.0:

--- src/main/java/com/alan/learning/App.java
+++ src/main/java/com/alan/learning/App.java
@@ -1,10 +1,21 @@
 package com.alan.learning;
 
-/**
- * Hello world!
- */
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Alien mars = new Alien();
+        mars.setAid(101);
+        mars.setAname("Alan");
+        mars.setColor("Green");
+
+
+        Configuration con = new Configuration();
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+
+        session.
     }
 }
